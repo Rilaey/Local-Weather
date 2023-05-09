c7bd7ef36aae487d5d208c0eb68182ade5bb8fe1
Riley <94302477+Rilaey@users.noreply.github.com>
2023-05-08 20:06:32 -0400

    local weather is currently working.

--- Local-Weather/src/App.jsx
+++ Local-Weather/src/App.jsx
@@ -1,20 +1,25 @@
 import { useState } from "react";
+import WeatherCard from "./components/WeatherCard";
 
 function App() {
-  const [search, setSearch] = useState("Orlando");
+  const [search, setSearch] = useState("");
+  const [weatherData, setWeatherData] = useState(null); // Added state variable
 
-  const apiKey = import.meta.env.VITE_API_KEY;
+  const apiKey = "a7d21723ff40c0e628b4d0449cd0708b";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}&units=imperial`
     );
 
     const data = await response.json();
 
-    console.log(data)
+    setSearch("");
+    setWeatherData(data); // Store data in state variable
+
+    console.log(data);
   };
 
   return (
@@ -28,12 +33,28 @@ function App() {
             type="text"
             placeholder="Enter a city..."
             className="input input-bordered w-full max-w-xs search-input"
+            value={search} // Bind input value to state variable
+            onChange={(e) => setSearch(e.target.value)} // Update search state on input change
           />
         </div>
         <div>
           <button className="btn btn-outline">Search</button>
         </div>
       </form>
+      <div>
+        {weatherData && ( // Render WeatherCard component if weatherData is available
+          <WeatherCard
+          iconCode={weatherData.weather[0].icon}
+          lowTemp={weatherData.main.temp_min}
+          highTemp={weatherData.main.temp_max}
+          currentTemp={weatherData.main.temp}
+          windSpeed={weatherData.wind.speed}
+          humidity={weatherData.main.humidity}
+          description={weatherData.weather[0].description.toUpperCase()}
+          cityName={weatherData.name}
+          />
+        )}
+      </div>
     </div>
   );
 }
--- Local-Weather/src/components/WeatherCard.jsx
+++ Local-Weather/src/components/WeatherCard.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import '../index.css'
 
-export default function WeatherCard({
-  icon,
+const WeatherCard = ({
+  iconCode,
   lowTemp,
   highTemp,
   currentTemp,
@@ -9,11 +10,52 @@ export default function WeatherCard({
   humidity,
   description,
   cityName
-}) {
+}) => {
+  const [iconUrl, setIconUrl] = useState("");
+
+  useEffect(() => {
+    const getIconUrl = async () => {
+      try {
+        const response = await fetch(
+          `https://openweathermap.org/img/wn/${iconCode}.png`
+        );
+        if (response.ok) {
+          const blob = await response.blob();
+          const url = URL.createObjectURL(blob);
+          setIconUrl(url);
+        } else {
+          // Handle error if icon retrieval fails
+          console.log("Failed to fetch weather icon.");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getIconUrl();
+  }, [iconCode]);
+
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className="flex items-center my-10">
+      <div>{iconUrl && <img src={iconUrl} className="icon-img" alt="Weather Icon" />}</div>
+
+      <div>
+        <div>
+          <div>City: {cityName}</div>
+          <div>Current: {currentTemp} °F</div>
+        </div>
+        <div>
+          <div>Low: {lowTemp} °F</div>
+          <div>High: {highTemp} °F</div>
+        </div>
+        <div>
+          <div>Wind Speed: {windSpeed} MPH</div>
+          <div>Humidity: {humidity} %</div>
+          <div>Description: {description}.</div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default WeatherCard;
